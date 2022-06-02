42024b9fe84fd231d5b9770360474f3646460d94
JPeng <alan36257407@gmail.com>
2022-06-02 15:49:56 +0800

    add git ignore

--- Meeting_Management/database.py
+++ Meeting_Management/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:gold90218@localhost:3306/meeting_management"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root@localhost:3306/meeting_management"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
