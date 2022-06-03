d356678ddfe6684e73473a3313c205be157ae667
JPeng <alan36257407@gmail.com>
2022-06-04 02:18:56 +0800

    Person CRUD almost fine

--- Meeting_Management/schemas.py
+++ Meeting_Management/schemas.py
@@ -1,18 +1,95 @@
-from typing import List, Optional
+from typing import Optional
 from pydantic import BaseModel
 from datetime import datetime
 from . import models
 
 
-class Person(BaseModel):
+class ExpertBase(BaseModel):
+    company_name: str = ""
+    job_title: str = ""
+    office_tel: str = ""
+    address: str = ""
+    bank_account: str = ""
+
+
+class AssistantBase(BaseModel):
+    office_tel: str = ""
+
+
+class DeptprofBase(BaseModel):
+    job_title: str = ""
+    office_tel: str = ""
+
+
+class OtherprofBase(BaseModel):
+    univ_name: str = ""
+    dept_name: str = ""
+    job_title: str = ""
+    office_tel: str = ""
+    address: str = ""
+    bank_account: str = ""
+
+
+class StudentBase(BaseModel):
+    student_id: str = ""
+    program: models.StudentProgramType
+    study_year: models.StudentStudyYearType
+
+
+class Expert(ExpertBase):
+
+    class Config:
+        orm_mode = True
+
+
+class Assistant(AssistantBase):
+
+    class Config:
+        orm_mode = True
+
+
+class Deptprof(DeptprofBase):
+
+    class Config:
+        orm_mode = True
+
+
+class Otherprof(OtherprofBase):
+
+    class Config:
+        orm_mode = True
+
+
+class Student(StudentBase):
+
+    class Config:
+        orm_mode = True
+
+
+class PersonBase(BaseModel):
     name: str
     gender: models.GenderType = models.GenderType.Male
     phone: str
     email: str
     type: models.PersonType = models.PersonType.Expert
 
+
+class Person(PersonBase):
+    expert_info: Optional[Expert] = None
+    assistant_info: Optional[Assistant] = None
+    dept_prof_info: Optional[Deptprof] = None
+    other_prof_info: Optional[Otherprof] = None
+    student_info: Optional[Student] = None
+
     class Config:
         orm_mode = True
+
+
+class PersonShow(Person):
+    def dict(self, *args, **kwargs):
+        if kwargs and kwargs.get("exclude_none") is not None:
+            kwargs["exclude_none"] = True
+            return BaseModel.dict(self, *args, **kwargs)
 
 
 class Meeting(BaseModel):
@@ -20,7 +97,6 @@ class Meeting(BaseModel):
     type: models.MeetingType
     time: datetime
     location: str
-    # is_draft: bool
     chair_id: int
     chair_speech: str
     chair_confirmed: bool
@@ -28,5 +104,4 @@ class Meeting(BaseModel):
 
     class Config:
         orm_mode = True
-
 
