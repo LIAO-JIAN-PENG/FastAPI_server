feb1de88201ac0e7ca9771ec9d50cb989101a4f7
JPeng <alan36257407@gmail.com>
2022-06-06 02:50:15 +0800

    file router can add attachment to selected meeting_id

--- Meeting_Management/routers/file.py
+++ Meeting_Management/routers/file.py
@@ -2,15 +2,17 @@ import asyncio
 import os
 from typing import List
 import aiofiles
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, Depends
 from ..main import UPLOAD_FOLDER
-from ..schemas import Attachment
+from .. import database, models
+from sqlalchemy.orm import Session
 
 router = APIRouter(
     prefix='/file',
     tags=['File']
 )
 
+get_db = database.get_db
 
 # @router.get("/download/{meeting_id}")
 # async def download_files(meeting_id: int, files: List[UploadFile]):
@@ -23,6 +25,7 @@ router = APIRouter(
 #
 #     return {"filenames": [file.filename for file in files]}
 
+
 async def upload_file(meeting_id: int, file: UploadFile):
     out_file_path = os.path.join(UPLOAD_FOLDER, str(meeting_id) + '-' + file.filename)
 
@@ -30,13 +33,23 @@ async def upload_file(meeting_id: int, file: UploadFile):
         content = await file.read()  # async read
         await out_file.write(content)
 
-    return out_file_path
+    return {'filename': file.filename, 'file_path': out_file_path}
 
 
 @router.post("/upload/{meeting_id}")
-async def upload_files(meeting_id: int, files: List[UploadFile]):
+async def upload_files(meeting_id: int, files: List[UploadFile], db: Session = Depends(get_db)):
     file_tasks = [upload_file(meeting_id, file) for file in files]
 
-    out_put_paths = await asyncio.gather(*file_tasks, return_exceptions=True)
+    file_info = await asyncio.gather(*file_tasks, return_exceptions=True)
 
-    return out_put_paths
+    meeting = db.query(models.Meeting).get(meeting_id)
+
+    for info in file_info:
+        attachment = models.Attachment(filename=info['filename'],
+                                       file_path=info['file_path'])
+        meeting.attachments.append(attachment)
+
+    db.commit()
+    db.refresh(meeting)
+
+    return file_info
