ca49b157361fadbc2b97c2a6207f2e96f88c32db
Goldwin <a1085513@mail.nuk.edu.tw>
2022-06-06 16:00:52 +0800

    gold add meeting's faker

--- Meeting_Management/routers/factory.py
+++ Meeting_Management/routers/factory.py
@@ -1,9 +1,11 @@
 from typing import List
 from fastapi import APIRouter, Depends
 from . import person as person_route
+from . import meeting as meeting_route
 from .. import database, schemas, models
 from sqlalchemy.orm import Session
-
+from datetime import datetime
+from datetime import timedelta
 from faker import Faker
 import random
 
@@ -75,5 +77,74 @@ def create_people(count: int, db: Session = Depends(get_db)):
     return f"{count} people have created"
 
 
-# @router.get('/meeting/{count}', response_model=)
+@router.get('/meeting/{count}', response_model=List[schemas.Meeting])
+def gen_meetings(count: int, db: Session = Depends(get_db)):
 
+    meetings = []
+    while count:
+        person = db.query(models.Person).all()
+        random.shuffle(person)
+
+        meeting = schemas.Meeting(title=faker.sentence(), type=random.choice(list(models.MeetingType)),
+                                  time=datetime.utcnow() - timedelta(days=random.randint(0, 7),
+                                                                     hours=random.randint(0, 23),
+                                                                     minutes=random.randint(0, 59)),
+                                  location=faker.address(), chair_id=person.pop().id,
+                                  chair_speech=faker.text(), chair_confirmed=faker.boolean(chance_of_getting_true=50),
+                                  minute_taker_id=person.pop().id
+                                  )
+
+        meeting.attendees = []
+        meeting.announcements = []
+        meeting.extempores = []
+        meeting.motions = []
+        for _ in range(random.randrange(2, len(person), 1)):
+            attendance = schemas.Attendee(person_id=person.pop().id,
+                                          is_present=faker.boolean(chance_of_getting_true=50),
+                                          is_confirmed=faker.boolean(chance_of_getting_true=50),
+                                          is_member=faker.boolean(chance_of_getting_true=50))
+            meeting.attendees.append(attendance)
+
+        for _ in range(random.randint(0, 5)):
+            meeting.announcements.append(generate_announcement())
+
+        for _ in range(random.randint(0, 3)):
+            meeting.extempores.append(generate_extempore())
+
+        for _ in range(random.randint(1, 3)):
+            meeting.motions.append(generate_motion())
+
+        meetings.append(meeting)
+        count -= 1
+
+    return meetings
+
+
+@router.post('/meeting/{count}')
+def create_meetings(count: int, db: Session = Depends(get_db)):
+    for meetings in gen_meetings(count, db):
+        meeting_route.create_meeting(meetings, [], db)
+
+    return f"{count} meetings have created"
+
+
+def generate_announcement():
+    announcement = schemas.Announcement(
+        content=faker.paragraph(nb_sentences=random.randint(3, 5)))
+    return announcement
+
+
+def generate_extempore():
+    extempore = schemas.Extempore(
+        content=faker.paragraph(nb_sentences=random.randint(3, 5)))
+    return extempore
+
+
+def generate_motion():
+    motion = schemas.Motion(description=faker.sentence(),
+                            content=faker.paragraph(nb_sentences=random.randint(0, 5)),
+                            status=random.choice(list(models.MotionStatusType)),
+                            resolution=faker.paragraph(nb_sentences=random.randint(0, 3)),
+                            execution=faker.paragraph(nb_sentences=random.randint(0, 3))
+                            )
+    return motion
