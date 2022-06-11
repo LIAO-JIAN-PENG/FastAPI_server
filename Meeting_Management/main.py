e17e3b7582bf0a09e5c5d74dabbb069bbcb874eb
JPeng <alan36257407@gmail.com>
2022-06-11 11:09:51 +0800

    CORS Problem first try

--- Meeting_Management/main.py
+++ Meeting_Management/main.py
@@ -3,9 +3,26 @@ from . import models
 from .database import engine
 from os import path
 from pathlib import Path
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# TODO: change here origin
+origins = [
+    "http://localhost.meetingapi.com",
+    "https://localhost.meetingapi.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://10.24.52.22/:1"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 UPLOAD_FOLDER = path.join(app.root_path, 'static', 'uploads')
 Path(UPLOAD_FOLDER).mkdir(parents=True, exist_ok=True)
