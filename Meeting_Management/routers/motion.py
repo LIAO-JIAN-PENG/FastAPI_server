377bf9a909d70aeca99cef265b64d8543b4290c3
JPeng <alan36257407@gmail.com>
2022-06-15 19:06:47 +0800

    change name get motion method name

--- Meeting_Management/routers/motion.py
+++ Meeting_Management/routers/motion.py
@@ -15,7 +15,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 @router.get('/', response_model=List[schemas.Motion])
-def get_all(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+def get_current(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     current_user_email = oauth2.get_current_user(token=token)
     user = db.query(models.Person).filter(models.Person.email == current_user_email).first()
     if user.type != '系助理' and user.email != 'admin@admin':
